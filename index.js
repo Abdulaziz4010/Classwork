50ace3b07451658c5dbd1cb5b673d748d5a3a8b4
Abdulaziz4010 <abdulazizs707ja@gamil.com>
2024-07-09 09:05:24 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1. Ixtiyoriy arrow function yarating va unga beriladigan argument promt yordamida kiritilgan son bo'lsin. Kiritilgan sonning raqamlari yig'indisi topilsin
+
+// const sumOfDigits = (num) => {
+//   const digits = num.toString().split("");
+
+//   const sum = digits.reduce((acc, digit) => acc + Number(digit), 0);
+
+//   return sum;
+// };
+
+// const num = prompt("Bir son kiriting:");
+// const result = sumOfDigits(num);
+
+// console.log(`Berilgan sonning raqamlari yig'indisi: ${result}`);
+
+// const isPalindrome = (str) => {
+//     const cleanedStr = str.toLowerCase().replace(/\s+/g, '');
+
+//     const reversedStr = cleanedStr.split('').reverse().join('');
+
+//     return cleanedStr === reversedStr;
+//   };
+
+//   const word = prompt("Bir so'z kiriting:");
+//   const result = isPalindrome(word);
+
+//   if (result) {
+//     console.log(`"${word}" so'zi palindrome hisoblanadi.`);
+//   } else {
+//     console.log(`"${word}" so'zi palindrome hisoblanmaydi.`);
+//   }
+
+// 2. Ixtiyoriy arrow function yarating va uning parametriga bi nechta sonlar berilsin. va o'sha sonlar yig'indisini arrayning element lari ko'rinishida ifodalang: masalan: [1, 22, 3] yigindisi: 26 result=> [2,6] 3.Promt yordamida ixtiyoriy son kiritilsin va o'sha sonning lvadradi palindrome yoki palindrome emasligi topilsin algorithm function ichida bo'lsin 4.Ixtiyoriy 2 ta funciton yaratilsin 1-function ichida random sonning raqamlari yig'indisi topilsin va mana shu 1-function 2-funciton ni ichida ishlasin masala callback function yordamida bajarilsin 5.Promt va funciton yarating promt ichida yozilgan so'zning palindome yoki palindrome emasligi topilsin 6. Ixtiyoriy object yarating => {a:1,b:22} value larinig yig'indisini toping => 23 value larining yigindising yig'indisini toping => 2 + 3 = 5
